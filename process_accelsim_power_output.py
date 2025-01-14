1914db956e44ef982cb5633658d4ed85580e6721
Brandon Tran <bqtran2@wisc.edu>
2025-01-14 12:14:12 -0600

    update accelwattch McPat Printout parser

--- process_accelsim_power_output.py
+++ process_accelsim_power_output.py
@@ -3,15 +3,18 @@ import csv
 import sys
 from multiprocessing import Pool
 
-def process_file(input_file):
-    if not input_file.endswith('.out') or 'accelsim' not in input_file:
-        print(f"Input file is not an accelsim output file: {input_file}")
+def process_file(input_file, output_file_header='',kernel_launch_latency=0):
+    # print(input_file)
+    if '.out' not in input_file:
+        print(f"Input file is not an output file: {input_file}")
         return
     found_mcpat = False
     found_proc = False
+    has_const_static = False
     processor_info_list = []
     processor_info = []
     kernel_num = 1
+    sampled_cycles = 0
 
     # Define the power metrics
     power_metrics = [
@@ -24,6 +27,8 @@ def process_file(input_file):
         'Runtime Dynamic',
     ]
     row_header = [
+        'const_power_W', 
+        'static_power_W', 
         'technology_nm',
         'core_clock_freq_mhz',
         'area',
@@ -35,12 +40,22 @@ def process_file(input_file):
         'runtime_dynamic_W',
     ]
     
-    output_file = input_file.replace('.out', '_processed.csv')  # Generate output file name
-
+    output_file = f"{output_file_header}.processed_csv"
         
     # Open the input file (with multiple kernels in it)
     with open(input_file, 'r') as infile:
         for line in infile:
+            ############################
+            # TODO If I update the print_dump to also print static power, I should have it here
+            ############################
+            if line.startswith("Constant Power: "):
+                has_const_static = True
+                const_power = line.strip().split("Power: ")[-1]
+                processor_info.append(const_power)
+            if line.startswith("Static Power: "):
+                static_power = line.strip().split("Power: ")[-1]
+                processor_info.append(static_power)
+
             # Check if the line contains "mcpat" and set found_mcpat to True if found
             if 'mcpat' in line.lower():
                 found_mcpat = True
@@ -68,12 +83,16 @@ def process_file(input_file):
                     value = value.replace(' mm^2','').replace(' W','')
                 if metric in power_metrics:
                     processor_info.append(value)
-            #I've finished getting data for one sample add it and go to next
+            #I've finished getting data from the processor component; I consider data collection for this phase finished
             elif found_mcpat and found_proc:
+                sampled_cycles += 1
                 found_mcpat = False
                 found_proc = False
-                processor_info_list.append(processor_info)
+                #Ignore cycles that just wait because of kernel launch latency
+                if sampled_cycles > kernel_launch_latency:
+                    processor_info_list.append(processor_info)
                 processor_info = []
+
             #These will come at the end of the kernel log
             elif 'gpu_sim_cycle' in line:
                 sim_cycle = line.strip().split()[-1]
@@ -84,6 +103,9 @@ def process_file(input_file):
                 kernel_output_file = output_file.replace('processed', f'processed_k{kernel_num}_{sim_cycle}cycle_{sim_instr}instr')
                 with open(kernel_output_file, 'w', newline='') as csvfile:
                     writer = csv.writer(csvfile)
+                    if not has_const_static:
+                        row_header.pop('const_power_W')
+                        row_header.pop('static_power_W')
                     writer.writerow(row_header)  # Write CSV header
                     for row in processor_info_list:
                         writer.writerow(row)
@@ -91,10 +113,32 @@ def process_file(input_file):
                 kernel_num += 1
 
 
-def process_files(input_dir):
-    files = [os.path.join(input_dir, filename) for filename in os.listdir(input_dir)]
-    with Pool() as pool:
-        pool.map(process_file, files)
+def process_input_dir(input_dir):
+    """
+    Loops through the input directory to find simulation.out files and processes them.
+
+    Args:
+        input_dir (str): Path to the input directory containing opcode/config folders.
+    """
+    kernel_launch_latency = 0
+    for opcode_folder in os.listdir(input_dir):
+        opcode_path = os.path.join(input_dir, opcode_folder)
+        if not os.path.isdir(opcode_path):
+            continue  # Skip if it's not a directory
+        for config_folder in os.listdir(opcode_path):
+            config_path = os.path.join(opcode_path, config_folder)            
+            if not os.path.isdir(config_path):
+                continue  # Skip if it's not a directory
+            else:
+                kernel_launch_latency = 5000
+                if "Tesla" in config_folder:
+                    kernel_launch_latency = 8606
+            simulation_file = os.path.join(config_path, "accelwattch_simulation.output")
+            if os.path.isfile(simulation_file):
+                # Pass the file, opcode, and config to the processing function
+                output_file_header = f"{opcode_folder}_{config_folder}"
+                process_file(simulation_file, output_file_header, kernel_launch_latency)
+
                 
 def main():
     if len(sys.argv) < 2 or len(sys.argv) > 3:
