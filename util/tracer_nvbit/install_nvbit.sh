7c70045010d521bef3b5723bc0ad5ae9d070bb2c
BranTran <BranTran@node.v100-compiling.sinclair-gpupow-pg0.wisc.cloudlab.us>
2025-04-11 18:38:59 -0600

    fix: tracer to load origianl instead of ppcle version, have LD_PRELOAD instead of CUDA_Injection

--- util/tracer_nvbit/install_nvbit.sh
+++ util/tracer_nvbit/install_nvbit.sh
@@ -2,10 +2,16 @@ export BASH_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 DATA_SUBDIR="/data_dirs/"
 DATA_ROOT=$BASH_ROOT$DATA_SUBDIR
 
+rm -rf $BASH_ROOT/nvbit_release
+
 if [ ! -d $DATA_ROOT ]; then
-    wget https://github.com/NVlabs/NVBit/releases/download/1.5.3/nvbit-Linux-ppc64le-1.5.3.tar.bz2
-    tar -xf nvbit-Linux-ppc64le-1.5.3.tar.bz2 -C $BASH_ROOT
-    rm nvbit-Linux-ppc64le-1.5.3.tar.bz2
+wget https://github.com/NVlabs/NVBit/releases/download/v1.7.4/nvbit-Linux-x86_64-1.7.4.tar.bz2
+tar -xf nvbit-Linux-x86_64-1.7.4.tar.bz2 -C $BASH_ROOT
+rm nvbit-Linux-x86_64-1.7.4.tar.bz2
+# On Summit
+#    wget https://github.com/NVlabs/NVBit/releases/download/1.5.3/nvbit-Linux-ppc64le-1.5.3.tar.bz2
+#    tar -xf nvbit-Linux-ppc64le-1.5.3.tar.bz2 -C $BASH_ROOT
+#    rm nvbit-Linux-ppc64le-1.5.3.tar.bz2
 fi
 
 
--- util/tracer_nvbit/run_hw_trace.py
+++ util/tracer_nvbit/run_hw_trace.py
@@ -105,8 +105,10 @@ for bench in benchmarks:
 	# first we generate the traces (.trace and kernelslist files)
 	# then, we do post-processing for the traces and generate (.traceg and kernelslist.g files)
 	# then, we delete the intermediate files ((.trace and kernelslist files files)
+        # Using CUDA_INJECT DIDN'T WORK FOR ME on Cloudlab (old bare metal machine)
+        # "export TRACES_FOLDER="+ this_trace_folder + "; CUDA_INJECTION64_PATH=" + os.path.join(nvbit_tracer_path, "tracer_tool.so") + " " +\
         sh_contents += "\nexport CUDA_VERSION=\"" + cuda_version + "\"; export CUDA_VISIBLE_DEVICES=\"" + options.device_num + "\" ; " +\
-            "export TRACES_FOLDER="+ this_trace_folder + "; CUDA_INJECTION64_PATH=" + os.path.join(nvbit_tracer_path, "tracer_tool.so") + " " +\
+            "export TRACES_FOLDER="+ this_trace_folder + "; LD_PRELOAD=" + os.path.join(nvbit_tracer_path, "tracer_tool.so") + " " +\
             exec_path + " " + str(args) + " ; " + os.path.join(nvbit_tracer_path,"traces-processing", "post-traces-processing") + " " +\
             os.path.join(this_trace_folder, "kernelslist") + " ; rm -f " + this_trace_folder + "/*.trace ; rm -f " + this_trace_folder + "/kernelslist "
 
