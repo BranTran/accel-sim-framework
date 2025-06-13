5cd0c1f818a8399712e00b5d08d71bbdb0953428
Brandon Tran <bqtran2@wisc.edu>
2025-06-13 11:40:46 -0500

    attempt quad solve [Abandoned]

--- util/accelwattch/quadprog_solver.m
+++ util/accelwattch/quadprog_solver.m
@@ -27,67 +27,59 @@
 %ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 %POSSIBILITY OF SUCH DAMAGE.
 
-input = csvread('accelwattch_volta_sass_sim.csv');
-A = input(:,1:31); % change 30 to number of power counters if different
-b = input(:,32);
-l = 0.1*ones(1,31); % lower bounds
-u = 1000*ones(1,31); % upper bounds
+input = csvread('accelwattch_volta_sass_sim.csv',1,1);
+counters = 26;
+A = input(:,1:counters-1); % change 30 to number of power counters if different
+b = input(:,counters);
+l = 0.1*ones(1,counters-1); % lower bounds
+u = 1000*ones(1,counters-1); % upper bounds
 
-M= zeros(1,31);
+M= zeros(1,counters-1);
 N= [0];
 
 
 
-C = zeros(16,31);
-D = zeros(16,1);
+C = zeros(10,counters-1);
+D = zeros(10,1);
 %These factors are calculated using McPAT per instruction energies * current AccelWattch scaling factors. They need to be changed after each iteration of the solver + simulator runs.
-C(1,8)=1;
-C(1,9)=-1.843582172; %INT <= FPU
+C(1,7)=1;
+C(1,8)=-1.843582172; %INT <= FPU
 
-C(2,9)=1;
-C(2,10)=-0.999991619; %FPU <= DPU
+C(2,8)=1;
+C(2,9)=-0.999991619; %FPU <= DPU
 
-C(3,8)=1;
-C(3,11)=-1.107050869; %INT <= INT_MUL24
+C(3,7)=1;
+C(3,10)=-1.000003101; %INT <= INT_MUL
 
-C(4,8)=1;
-C(4,12)=-1.000003101; %INT <= INT_MUL32
+C(4,11)=1;
+C(4,16)=-1.063781571; %FP_MUL <= DP_MUL
 
-C(5,8)=1;
-C(5,13)=-1.00000271; %INT <= INT_MUL
+C(5,11)=1;
+C(5,12)=-5.587170605; %FP_MUL <= FP_SQRT
 
-C(8,15)=1;
-C(8,16)=-14.17176203; %FP_MUL <= FP_DIV
+C(6,11)=1;
+C(6,13)=-2.082920111; %FP_MUL <= FP_LG
 
-C(9,15)=1;
-C(9,21)=-1.063781571; %FP_MUL <= DP_MUL
+C(7,11)=1;
+C(7,14)=-1.767684722; %FP_MUL <= FP_SIN
 
-C(10,15)=1;
-C(10,17)=-5.587170605; %FP_MUL <= FP_SQRT
+C(8,11)=1;
+C(8,15)=-1.438999757; %FP_MUL <= FP_EXP
 
-C(11,15)=1;
-C(11,18)=-2.082920111; %FP_MUL <= FP_LG
+C(9,11)=1;
+C(9,17)=-75.07256801; %FP_MUL <= TENSOR
 
-C(12,15)=1;
-C(12,19)=-1.767684722; %FP_MUL <= FP_SIN
-
-C(13,15)=1;
-C(13,20)=-1.438999757; %FP_MUL <= FP_EXP
-
-C(15,15)=1;
-C(15,23)=-75.07256801; %FP_MUL <= TENSOR
-
-C(16,15)=1;
-C(16,24)=-0.999997535; %FP_MUL <= TEXP
+C(10,11)=1;
+C(10,18)=-0.999997535; %FP_MUL <= TEXP
 
 %The Idle_Core_power, static power, constant power components are already
 %modeled prior to dynamic power quadprog optimization
-l(29)=1;
-u(29)=1;
-l(30)=1;
-u(30)=1;
-l(31)=1;
-u(31)=1;
+l(counters-3)=1;
+u(counters-3)=1;
+l(counters-2)=1;
+u(counters-2)=1;
+l(counters-1)=1;
+u(counters-1)=1;
 
 result = quadprog(2*A'*A, -2*A'*b, C, D, [], [], l, u);
 csvwrite('scaled_coefficients.csv', result);
